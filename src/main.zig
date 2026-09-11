3d494b847dd0e4fefa6eeb9eb8a062866b84abc9
Pranit <pranit@vercel.com>
2026-09-11 19:31:12 -0400

    Reduce input-state initialization data

--- src/main.zig
+++ src/main.zig
@@ -609,6 +609,7 @@ const App = struct {
     ) !Self {
         var app = Self{
             .alloc = alloc,
+            .input_runtime = undefined,
             .auth = undefined,
             .usage_dashboard = undefined,
             .session_persistence = undefined,
@@ -623,6 +624,7 @@ const App = struct {
             else
                 shell_process_provider.provider,
         };
+        InputRuntime.initInto(&app.input_runtime);
         auth_runtime.Runtime.initIntoWithMode(
             &app.auth,
             app_api_key_validator,
